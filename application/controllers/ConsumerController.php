c9139116ea539844182b24460a02d8ef9b967313
Zed <alexandre@editoraquantum.com.br>
2011-05-30 12:28:54 -0300

    * Resolvido o problema do OpenId Consumer

--- application/controllers/ConsumerController.php
+++ application/controllers/ConsumerController.php
@@ -1,7 +1,19 @@
 <?php
-
+/**
+ * @category   Zend
+ * @package    Zend_OpenId
+ * @subpackage Demos
+ * @uses       Zend_Controller_Action
+ * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 class ConsumerController extends Zend_Controller_Action
 {
+	public function init()
+	{
+    	$this->_helper->layout->disableLayout();
+	}
+
     /**
      * indexAction
      *
@@ -26,7 +38,7 @@ class ConsumerController extends Zend_Controller_Action
     {
         $auth = Zend_Auth::getInstance();
         if (!$auth->hasIdentity()) {
-            //$this->_redirect('/default/consumer/login');
+            $this->_redirect('index/login');
         }
         $this->view->user = $auth->getIdentity();
     }
@@ -38,38 +50,53 @@ class ConsumerController extends Zend_Controller_Action
      */
     public function loginAction()
     {
-        if (isset($_POST['openid_action']) &&
-            $_POST['openid_action'] == "login" &&
-            !empty($_POST['openid_identifier'])) 
-        {
- 
-            $consumer = new Zend_OpenId_Consumer();
-            if (!$consumer->login($_POST['openid_identifier'])) 
-            {
-                $status = $consumer->getError();
-            }
-        } else if (isset($_GET['openid_mode'])) 
-        {
-            if ($_GET['openid_mode'] == "id_res") 
-            {
-                $consumer = new Zend_OpenId_Consumer();
-                if ($consumer->verify($_GET, $id)) {
-				    $this->_redirect('/'.$this->_getParam('module').'/consumer/welcome');
-                    
-                } else {
-                    $status = "INVALID " . htmlspecialchars($id);
+        $this->view->status = "";
+        if (($this->_request->isPost() &&
+             $this->_request->getPost('openid_action') == 'login' &&
+             $this->_request->getPost('openid_identifier', '') !== '') ||
+            ($this->_request->isPost() &&
+             $this->_request->getPost('openid_mode') !== null) ||
+            (!$this->_request->isPost() &&
+             $this->_request->getQuery('openid_mode') != null)) 
+		{
 
-                }
-            } else if ($_GET['openid_mode'] == "cancel") {
-                $status = "CANCELLED";
-            }
-        }
-	    $this->view->status .= "$status<br>\n";
-    }
+			$client = new Zend_Http_Client($this->_request->getPost('openid_identifier'));
+
+			//Extensions
+			$sreg = new Zend_OpenId_Extension_Sreg(array(
+														 'nickname'=>true,
+														 'email'=>false,
+														 'fullname'=>false,
+														 'dob'=>false,
+														 'gender'=>false,
+														 'postcode'=>false,
+														 'country'=>false,
+														 'language'=>false,
+														 'timezone'=>false
+														 ), null, 1.1);
 
 
+			$openId = new Zend_Auth_Adapter_OpenId($this->_request->getPost('openid_identifier'));
+			$openId->setHttpClient($client);
+			$openId->setExtensions($sreg);
 
-    
+			$auth = Zend_Auth::getInstance();
+			$result = $auth->authenticate($openId);
+
+			if ($result->isValid()) {
+				//$session = $auth->getStorage();
+				//$session->write($sreg->getProperties());
+				$this->_redirect('/'.$this->_getParam('module').'/consumer/welcome');
+			} else {
+				$auth->clearIdentity();
+				foreach ($result->getMessages() as $message) {
+					$this->view->status .= "$message<br>\n";
+				}
+			}
+		}
+		$this->render();
+	}
+
 	/**
 	 * logoutAction
 	 *
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,8 +10,12 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
-    }
+		// Código para responder ao OpenID
+		if($this->_getParam('openid_mode')){
+			$this->_forward('handle','provider','default');
+		}
+
+	}
 
 
 }
